ca810b36a58007f93a8d1b3afa26fa6b8bb74aef
cherrylzhao <zhaojunv3@126.com>
2019-05-06 10:52:39 +0800

    add maven assembly for onlinebank.

--- sharding-jdbc-test/jdbc-onlinebank-test/src/main/java/io/shardingsphere/example/jdbc/onlinebank/SpringBootStarterExample.java
+++ sharding-jdbc-test/jdbc-onlinebank-test/src/main/java/io/shardingsphere/example/jdbc/onlinebank/SpringBootStarterExample.java
@@ -19,9 +19,8 @@ package io.shardingsphere.example.jdbc.onlinebank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.transaction.jta.JtaAutoConfiguration;
 
-@SpringBootApplication(exclude = JtaAutoConfiguration.class)
+@SpringBootApplication
 public class SpringBootStarterExample {
     
     public static void main(final String[] args) {
